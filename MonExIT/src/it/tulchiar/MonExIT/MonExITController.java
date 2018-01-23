3c0470be3e80e36b71a5caf5c2c30dcd2f0c51f0
tulchiar <tulchiar@gmail.com>
2018-01-23 14:31:44 +0100

    Modificata l'interfaccia grafica, adesso funzionante

--- MonExIT/src/it/tulchiar/MonExIT/MonExITController.java
+++ MonExIT/src/it/tulchiar/MonExIT/MonExITController.java
@@ -4,6 +4,7 @@
 
 package it.tulchiar.MonExIT;
 
+
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -11,13 +12,18 @@ import java.util.ResourceBundle;
 import it.tulchiar.MonExIT.model.Model;
 import it.tulchiar.MonExIT.model.Valuta;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class MonExITController {
 	
 	Model model;
+	Boolean editabile = true;
+	
 	
 	public void setModel(Model model) {
 		this.model = model;
@@ -32,8 +38,14 @@ public class MonExITController {
     @FXML // fx:id="txtImporto"
     private TextField txtImporto; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtData"
-    private TextField txtData; // Value injected by FXMLLoader
+    @FXML // fx:id="txtGiorno"
+    private TextField txtGiorno; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="txtMese"
+    private TextField txtMese; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="txtAnno"
+    private TextField txtAnno; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtControvalore"
     private TextField txtControvalore; // Value injected by FXMLLoader
@@ -43,24 +55,57 @@ public class MonExITController {
 
     @FXML
     void doCalcola(ActionEvent event) {
-    	try {
-	    	Double tassoDiCambio = model.getTassoDiCambio(new Valuta("USD"), new Valuta("EUR"), LocalDate.parse(txtData.getText()));
-	    	Double importoA = Double.parseDouble(txtImporto.getText());
-	    	if(importoA != 0.0) {
-	    		txtControvalore.setText(((Double)(importoA / tassoDiCambio)).toString());
+	    	try {
+	    		
+	    		String giorno = txtGiorno.getText();
+	    		String mese = txtMese.getText();
+	    		String anno = txtAnno.getText();
+	    		
+	    		LocalDate data = LocalDate.parse(anno+"-"+mese+"-"+giorno);
+	    		
+		    	Double tassoDiCambio = model.getTassoDiCambio(new Valuta("USD"), new Valuta("EUR"), data);
+		    	Double importoA = Double.parseDouble(txtImporto.getText());
+		    	if(importoA != 0.0) {
+		    		txtControvalore.setText(((Double)(importoA / tassoDiCambio)).toString());
+		    	}
+	    	} catch(Exception e) {
+	    		System.err.println("Errore: " + e.getMessage());
 	    	}
-    	} catch(Exception e) {
-    		System.err.println("Errore: " + e.getMessage());
-    	}
     }
 
+    public void setup(){
+    		txtGiorno.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>() {
+
+				@Override
+				public void handle(Event event) {
+					
+					if(editabile) {
+						txtMese.setDisable(true);
+						txtAnno.setDisable(true);
+						editabile = false;
+					} else {
+						txtMese.setDisable(false);
+						txtAnno.setDisable(false);
+						editabile = true;
+					}
+					
+					
+				}
+			});
+    }
+    
+    
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
     	
+    	setup();
     	btnCalcola.setDefaultButton(true);
     	
+    	
         assert txtImporto != null : "fx:id=\"txtImporto\" was not injected: check your FXML file 'MonExIT.fxml'.";
-        assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'MonExIT.fxml'.";
+        assert txtGiorno != null : "fx:id=\"txtGiorno\" was not injected: check your FXML file 'MonExIT.fxml'.";
+        assert txtMese != null : "fx:id=\"txtMese\" was not injected: check your FXML file 'MonExIT.fxml'.";
+        assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'MonExIT.fxml'.";
         assert txtControvalore != null : "fx:id=\"txtControvalore\" was not injected: check your FXML file 'MonExIT.fxml'.";
         assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'MonExIT.fxml'.";
 
