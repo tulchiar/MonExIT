6b5690e2bbe7668268b0bcf449d381bf7c0aa605
marco <marco@marco-ubuntu>
2018-01-23 22:53:21 +0100

    Modificata l'interfaccia grafica e le funzioni principali

--- MonExIT/src/it/tulchiar/MonExIT/Main.java
+++ MonExIT/src/it/tulchiar/MonExIT/Main.java
@@ -24,7 +24,12 @@ public class Main extends Application {
 			
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+						
 			primaryStage.setScene(scene);
+			primaryStage.setAlwaysOnTop(true);
+			
+			controller.setScene(scene);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- MonExIT/src/it/tulchiar/MonExIT/MonExITController.java
+++ MonExIT/src/it/tulchiar/MonExIT/MonExITController.java
@@ -15,19 +15,31 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class MonExITController {
 	
 	Model model;
+	Scene scene;
 	Boolean editabile = true;
 	
 	
 	public void setModel(Model model) {
 		this.model = model;
 	}
+	
+	public void setScene(Scene scene) {
+		this.scene = scene;
+	}
+	
 	
 	@FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -47,11 +59,17 @@ public class MonExITController {
     @FXML // fx:id="txtAnno"
     private TextField txtAnno; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtControvalore"
-    private TextField txtControvalore; // Value injected by FXMLLoader
+    @FXML // fx:id="lblControvalore"
+    private Label lblControvalore; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="lblCambio"
+    private Label lblCambio; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnCalcola"
     private Button btnCalcola; // Value injected by FXMLLoader
+    
+    Clipboard clipboard;
+    ClipboardContent clipboardContent;
 
     @FXML
     void doCalcola(ActionEvent event) {
@@ -60,23 +78,28 @@ public class MonExITController {
 	    		String giorno = txtGiorno.getText();
 	    		String mese = txtMese.getText();
 	    		String anno = txtAnno.getText();
+	    		Double controvalore = 0.0;
 	    		
 	    		LocalDate data = LocalDate.parse(anno+"-"+mese+"-"+giorno);
 	    		
 		    	Double tassoDiCambio = model.getTassoDiCambio(new Valuta("USD"), new Valuta("EUR"), data);
 		    	Double importoA = Double.parseDouble(txtImporto.getText());
 		    	if(importoA != 0.0) {
-		    		txtControvalore.setText(((Double)(importoA / tassoDiCambio)).toString());
+		    		controvalore = (Double)(importoA / tassoDiCambio);
+		    		lblCambio.setText("(cambio "+ String.format("%.4f", tassoDiCambio)+")");
+		    		lblControvalore.setText(String.format("%.2f", controvalore));
 		    	}
 	    	} catch(Exception e) {
 	    		System.err.println("Errore: " + e.getMessage());
 	    	}
     }
 
-    public void setup(){
+    
+    
+    public void setup(){    	
     		txtGiorno.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>() {
-
-				@Override
+				
+    			@Override
 				public void handle(Event event) {
 					
 					if(editabile) {
@@ -92,6 +115,17 @@ public class MonExITController {
 					
 				}
 			});
+    		lblControvalore.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>(){
+
+				@Override
+				public void handle(Event arg0) {
+					clipboardContent.putString(lblControvalore.getText());
+					clipboard.setContent(clipboardContent);
+					
+				}
+    			
+    		});
+
     }
     
     
@@ -100,13 +134,15 @@ public class MonExITController {
     	
     	setup();
     	btnCalcola.setDefaultButton(true);
-    	
+    	clipboard = Clipboard.getSystemClipboard();
+    	clipboardContent = new ClipboardContent();
     	
         assert txtImporto != null : "fx:id=\"txtImporto\" was not injected: check your FXML file 'MonExIT.fxml'.";
         assert txtGiorno != null : "fx:id=\"txtGiorno\" was not injected: check your FXML file 'MonExIT.fxml'.";
         assert txtMese != null : "fx:id=\"txtMese\" was not injected: check your FXML file 'MonExIT.fxml'.";
         assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'MonExIT.fxml'.";
-        assert txtControvalore != null : "fx:id=\"txtControvalore\" was not injected: check your FXML file 'MonExIT.fxml'.";
+        assert lblControvalore != null : "fx:id=\"lblControvalore\" was not injected: check your FXML file 'MonExIT.fxml'.";
+        assert lblCambio != null : "fx:id=\"lblCambio\" was not injected: check your FXML file 'MonExIT.fxml'.";
         assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'MonExIT.fxml'.";
 
     }
